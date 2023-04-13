7ebc10fea3559e96b25e78b21bdd617455f7dd96
karamjeetinstep <70193618+karamjeetinstep@users.noreply.github.com>
2023-04-13 18:10:04 +0530

    default select

--- src/common/CheckBox/index.js
+++ src/common/CheckBox/index.js
@@ -9,6 +9,7 @@ const CheckBox = (props) => {
           type="checkbox"
           name="checkbox"
           value=""
+          defaultChecked={props.defaultSelected.includes(item.id)}
           onChange={(e)=>{props.onClick(e.target.checked,item.id)}}
         />
         <p>{item.name} &#40;<span>{item.length}</span>&#41;</p>
