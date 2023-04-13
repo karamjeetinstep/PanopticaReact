07de80cda92f7c2b7a7a670fac587451e089d56d
Sunder <sunder@silstonegroup.com>
2023-04-13 12:13:39 +0530

    congratulation design

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -4,18 +4,20 @@ const Button = (props) => {
     <button
       style={{
         padding: props.padding,
-        color:props.disabled?"#B2B2B2":props.color,
+        color: props.disabled ? "#B2B2B2" : props.color,
         margin: props.margin,
         height: props.height,
-        border:props.border,
-        background:props.disabled?"#DEDEDE": props.background,
+        border: props.border,
+        background: props.disabled ? "#DEDEDE" : props.background,
         borderRadius: props.borderRadius,
-        cursor:props.disabled?"unset":props.cursor,
-        width:props.width,
+        cursor: props.disabled ? "unset" : props.cursor,
+        width: props.width,
       }}
       disabled={props.disabled}
-      onClick={()=>{props.onClick()}}
-      className={props.className}
+      onClick={() => {
+        props.onClick();
+      }}
+      className={`btn-defult ${props.className}`}
     >
       {props.text}
     </button>
@@ -23,16 +25,18 @@ const Button = (props) => {
 };
 Button.defaultProps = {
   text: "button",
-  padding:"0 3%",
-  color:"#f7f7f7",
+  padding: "0 3%",
+  color: "#f7f7f7",
   margin: "0",
-  height:'38px',
-  border:"none",
-  background:"#007aa3",
-  borderRadius:"30px",
-  cursor: 'pointer',
-  width:"fit-content",
-  onClick:()=>{console.log("send your onClick function in button props")},
-  disabled:false
+  height: "38px",
+  border: "none",
+  background: "#007aa3",
+  borderRadius: "30px",
+  cursor: "pointer",
+  width: "fit-content",
+  onClick: () => {
+    console.log("send your onClick function in button props");
+  },
+  disabled: false,
 };
 export default Button;
--- src/components/EvaluateAPI/rightModal.js
+++ src/components/EvaluateAPI/rightModal.js
@@ -2,42 +2,44 @@ import React from "react";
 import crossIcon from "../../assets/images/crossIcon.png";
 const RightModal = (props) => {
   return (
-    <div className="rightModal">
-      <div className="modalHeading">
-        <h3>Find out more</h3>
-        <button className="closeBotton" onClick={props.onClick}>
-          <img alt="" src={crossIcon}></img>
-        </button>
+    <div className={`rightModal ${props.isOpen ? "open" : "close"}`}>
+      <div className={`bas ${props.isOpen ? "d-block" : "d-none"}`}>
+        <div className="modalHeading">
+          <h3>Find out more</h3>
+          <button className="closeBotton" onClick={props.onClick}>
+            <img alt="" src={crossIcon}></img>
+          </button>
+        </div>
+        <h5 className="heading">This is a long title to test this</h5>
+        <p className="detailtext">
+          The accordion component delivers large amounts of content in a small
+          space through progressive disclosure. The user gets key details about
+          the underlying content and can choose to expand.
+        </p>
+        <h5 className="heading">This is a long title to test this</h5>
+        <p className="detailtext">
+          The accordion component delivers large amounts of content in a small
+          space through progressive disclosure. The user gets key details about
+          the underlying content and can choose to expand that content within
+          the constraints of the accordion.
+        </p>
+        <h5 className="heading">This is a long title to test this</h5>
+        <p className="detailtext">
+          The accordion component delivers large amounts of content in a small
+          space through progressive disclosure. The user gets key details about
+          the underlying content and can choose to expand that content within
+          the constraints of the accordion. Accordions work especially well on
+          mobile interfaces or whenever vertical space is at a premium.
+        </p>
+        <h5 className="heading">This is a long title to test this</h5>
+        <p className="detailtext">
+          The accordion component delivers large amounts of content in a small
+          space through progressive disclosure. The user gets key details about
+          the underlying content and can choose to expand that content within
+          the constraints of the accordion. Accordions work especially well on
+          mobile interfaces or whenever vertical space is at a premium.
+        </p>
       </div>
-      <h5 className="heading">This is a long title to test this</h5>
-      <p className="detailtext">
-        The accordion component delivers large amounts of content in a small
-        space through progressive disclosure. The user gets key details about
-        the underlying content and can choose to expand.
-      </p>
-      <h5 className="heading">This is a long title to test this</h5>
-      <p className="detailtext">
-        The accordion component delivers large amounts of content in a small
-        space through progressive disclosure. The user gets key details about
-        the underlying content and can choose to expand that content within the
-        constraints of the accordion.
-      </p>
-      <h5 className="heading">This is a long title to test this</h5>
-      <p className="detailtext">
-        The accordion component delivers large amounts of content in a small
-        space through progressive disclosure. The user gets key details about
-        the underlying content and can choose to expand that content within the
-        constraints of the accordion. Accordions work especially well on mobile
-        interfaces or whenever vertical space is at a premium.
-      </p>
-      <h5 className="heading">This is a long title to test this</h5>
-      <p className="detailtext">
-        The accordion component delivers large amounts of content in a small
-        space through progressive disclosure. The user gets key details about
-        the underlying content and can choose to expand that content within the
-        constraints of the accordion. Accordions work especially well on mobile
-        interfaces or whenever vertical space is at a premium.
-      </p>
     </div>
   );
 };
